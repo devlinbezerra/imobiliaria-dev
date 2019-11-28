7bcc996650e6f14566668d78b40462e49986417f
Devlin Bezerra <devlin.bezerra@gmail.com>
2019-11-28 10:30:38 -0400

    Inclusão de relatório de débitos. Build.

--- assets/scripts/model.js
+++ assets/scripts/model.js
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
-export default (()=> {
+export default (() => {
+	/**
+	 * proxys
+	 * https://crossorigin.me/
+	 * https://cors-anywhere.herokuapp.com/
+	 *
+	 */
+	const proxy = 'https://crossorigin.me/';
+	const local = '64.31.48.87';
 
-    return {
-        //prop é uma objeto com data (json com dados), tabela e acao (inserir, alterar, etc)
-        db: (prop) => axios.post(`http://localhost/facil/api/${prop.acao}/${prop.tabela}`, prop.data),
-        auth: (data) => axios.post(`http://localhost/facil/api/autenticar`, data)
-    }
+	return {
+		//prop é uma objeto com data (json com dados), tabela e acao (inserir, alterar, etc)
+		db: prop =>
+			axios.post(
+				`${proxy}http://${local}/facil/api/${prop.acao}/${prop.tabela}`,
+				prop.data
+			),
+		auth: data => axios.post(`http://${local}/facil/api/autenticar`, data)
+	};
 })();
