23fa0f8a481a92de8ee3f208a4f7d25e4e4f3bc2
Devlin Bezerra <devlin.bezerra@gmail.com>
2019-11-29 08:41:34 -0400

    Correção proxy devido CORS policy

--- assets/scripts/model.js
+++ assets/scripts/model.js
@@ -7,15 +7,20 @@ export default (() => {
 	 * https://cors-anywhere.herokuapp.com/
 	 *
 	 */
-	const proxy = 'https://crossorigin.me/';
+	const proxy = ''; //'https://crossorigin.me/';
 	const local = '64.31.48.87';
+	const config = {
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		crossdomain: true
+	};
 
 	return {
 		//prop é uma objeto com data (json com dados), tabela e acao (inserir, alterar, etc)
 		db: prop =>
 			axios.post(
 				`${proxy}http://${local}/facil/api/${prop.acao}/${prop.tabela}`,
-				prop.data
+				prop.data,
+				config
 			),
 		auth: data => axios.post(`http://${local}/facil/api/autenticar`, data)
 	};
